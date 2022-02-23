9e5da13ceef414b0de268e59e476381f3c6febeb
妙码生花 <18523774412@qq.com>
2022-02-23 19:11:08 +0800

    fix:安装检测优化

--- public/index.php
+++ public/index.php
@@ -18,23 +18,27 @@ namespace think;
  */
 $rootPath = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR;
 
-// 安装检测-s
-if (!is_file($rootPath . 'install.lock') && is_file($rootPath . 'install' . DIRECTORY_SEPARATOR . 'index.html')) {
-    header("location:" . DIRECTORY_SEPARATOR . 'install' . DIRECTORY_SEPARATOR);
-    exit();
-}
-// 安装检测-e
+if (substr($_SERVER['REQUEST_URI'], 1, 9) != 'index.php') {
+    // 没有入口文件=用户访问前端
 
-/*
- * 检测是否已编译前端-s
- * 如果存在 index.html 则访问 index.html
- * 本系统无需且不能配置隐藏 index.php 文件
- */
-if (substr($_SERVER['REQUEST_URI'], 1, 9) != 'index.php' && is_file($rootPath . 'index.html')) {
-    header("location:" . DIRECTORY_SEPARATOR . 'index.html');
-    exit();
+    // 安装检测-s
+    if (!is_file($rootPath . 'install.lock') && is_file($rootPath . 'install' . DIRECTORY_SEPARATOR . 'index.html')) {
+        header("location:" . DIRECTORY_SEPARATOR . 'install' . DIRECTORY_SEPARATOR);
+        exit();
+    }
+    // 安装检测-e
+
+    /*
+     * 检测是否已编译前端-s
+     * 如果存在 index.html 则访问 index.html
+     * 本系统无需且不能配置隐藏 index.php 文件
+     */
+    if (is_file($rootPath . 'index.html')) {
+        header("location:" . DIRECTORY_SEPARATOR . 'index.html');
+        exit();
+    }
+    // 检测是否已编译前端-e
 }
-// 检测是否已编译前端-e
 
 require __DIR__ . '/../vendor/autoload.php';
 
