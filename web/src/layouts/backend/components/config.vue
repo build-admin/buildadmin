401a4f454336e59e0b41a3c296ec6d17bdc02f44
妙码生花 <18523774412@qq.com>
2022-06-06 11:24:36 +0800

    refactor:布局配置的语言包整理

--- web/src/layouts/backend/components/config.vue
+++ web/src/layouts/backend/components/config.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="layout-config-drawer">
-        <el-drawer v-model="configStore.layout.showDrawer" title="布局配置" size="310px" @close="onCloseDrawer">
+        <el-drawer v-model="configStore.layout.showDrawer" :title="t('layouts.Layout configuration')" size="310px" @close="onCloseDrawer">
             <el-scrollbar class="layout-mode-style-scrollbar">
                 <el-form ref="formRef" :model="configStore.layout">
                     <div class="layout-mode-styles-box">
-                        <el-divider border-style="dashed">布局方式</el-divider>
+                        <el-divider border-style="dashed">{{ t('layouts.Layout mode') }}</el-divider>
                         <div class="layout-mode-box-style">
                             <el-row class="layout-mode-box-style-row" :gutter="10">
                                 <el-col :span="12">
@@ -20,7 +20,7 @@
                                                 <div class="layout-mode-style-container"></div>
                                             </div>
                                         </div>
-                                        <div class="layout-mode-style-name">默认</div>
+                                        <div class="layout-mode-style-name">{{ t('layouts.default') }}</div>
                                     </div>
                                 </el-col>
                                 <el-col :span="12">
@@ -36,7 +36,7 @@
                                                 <div class="layout-mode-style-container"></div>
                                             </div>
                                         </div>
-                                        <div class="layout-mode-style-name">经典</div>
+                                        <div class="layout-mode-style-name">{{ t('layouts.classic') }}</div>
                                     </div>
                                 </el-col>
                             </el-row>
@@ -53,18 +53,18 @@
                                                 <div class="layout-mode-style-container"></div>
                                             </div>
                                         </div>
-                                        <div class="layout-mode-style-name">单栏</div>
+                                        <div class="layout-mode-style-name">{{ t('layouts.Single column') }}</div>
                                     </div>
                                 </el-col>
                             </el-row>
                         </div>
-                        <el-divider border-style="dashed">全局</el-divider>
+                        <el-divider border-style="dashed">{{ t('layouts.overall situation') }}</el-divider>
                         <div class="layout-config-global">
-                            <el-form-item label="后台页面切换动画">
+                            <el-form-item :label="t('layouts.Background page switching animation')">
                                 <el-select
                                     @change="onCommitState($event, 'mainAnimation')"
                                     v-model="configStore.layout.mainAnimation"
-                                    placeholder="请选择动画名称"
+                                    :placeholder="t('layouts.Please select an animation name')"
                                 >
                                     <el-option label="slide-right" value="slide-right"></el-option>
                                     <el-option label="slide-left" value="slide-left"></el-option>
@@ -77,44 +77,44 @@
                             </el-form-item>
                         </div>
 
-                        <el-divider border-style="dashed">侧边栏</el-divider>
+                        <el-divider border-style="dashed">{{ t('layouts.sidebar') }}</el-divider>
                         <div class="layout-config-aside">
-                            <el-form-item label="侧边菜单栏背景色">
+                            <el-form-item :label="t('layouts.Side menu bar background color')">
                                 <el-color-picker @change="onCommitState($event, 'menuBackground')" v-model="configStore.layout.menuBackground" />
                             </el-form-item>
-                            <el-form-item label="侧边菜单文字颜色">
+                            <el-form-item :label="t('layouts.Side menu text color')">
                                 <el-color-picker @change="onCommitState($event, 'menuColor')" v-model="configStore.layout.menuColor" />
                             </el-form-item>
-                            <el-form-item label="侧边菜单激活项背景色">
+                            <el-form-item :label="t('layouts.Side menu active item background color')">
                                 <el-color-picker
                                     @change="onCommitState($event, 'menuActiveBackground')"
                                     v-model="configStore.layout.menuActiveBackground"
                                 />
                             </el-form-item>
-                            <el-form-item label="侧边菜单激活项文字色">
+                            <el-form-item :label="t('layouts.Side menu active item text color')">
                                 <el-color-picker @change="onCommitState($event, 'menuActiveColor')" v-model="configStore.layout.menuActiveColor" />
                             </el-form-item>
-                            <el-form-item label="显示侧边菜单顶栏(LOGO栏)">
+                            <el-form-item :label="t('layouts.Show side menu top bar (logo bar)')">
                                 <el-switch @change="onCommitState($event, 'menuShowTopBar')" v-model="configStore.layout.menuShowTopBar"></el-switch>
                             </el-form-item>
-                            <el-form-item label="侧边菜单顶栏背景色">
+                            <el-form-item :label="t('layouts.Side menu top bar background color')">
                                 <el-color-picker
                                     @change="onCommitState($event, 'menuTopBarBackground')"
                                     v-model="configStore.layout.menuTopBarBackground"
                                 />
                             </el-form-item>
-                            <el-form-item label="侧边菜单宽度(展开时)">
+                            <el-form-item :label="t('layouts.Side menu width (when expanded)')">
                                 <el-input @input="onCommitState($event, 'menuWidth')" type="number" :step="10" v-model="configStore.layout.menuWidth">
                                     <template #append>px</template>
                                 </el-input>
                             </el-form-item>
-                            <el-form-item label="侧边菜单默认图标">
+                            <el-form-item :label="t('layouts.Side menu default icon')">
                                 <selector @change="onCommitMenuDefaultIcon($event, 'menuDefaultIcon')" v-model="configStore.layout.menuDefaultIcon" />
                             </el-form-item>
-                            <el-form-item label="侧边菜单水平折叠">
+                            <el-form-item :label="t('layouts.Side menu horizontal collapse')">
                                 <el-switch @change="onCommitState($event, 'menuCollapse')" v-model="configStore.layout.menuCollapse"></el-switch>
                             </el-form-item>
-                            <el-form-item label="侧边菜单手风琴">
+                            <el-form-item :label="t('layouts.Side menu accordion')">
                                 <el-switch
                                     @change="onCommitState($event, 'menuUniqueOpened')"
                                     v-model="configStore.layout.menuUniqueOpened"
@@ -122,43 +122,46 @@
                             </el-form-item>
                         </div>
 
-                        <el-divider border-style="dashed">顶栏</el-divider>
+                        <el-divider border-style="dashed">{{ t('layouts.Top bar') }}</el-divider>
                         <div class="layout-config-aside">
-                            <el-form-item label="顶栏背景色">
+                            <el-form-item :label="t('layouts.Top bar background color')">
                                 <el-color-picker
                                     @change="onCommitState($event, 'headerBarBackground')"
                                     v-model="configStore.layout.headerBarBackground"
                                 />
                             </el-form-item>
-                            <el-form-item label="顶栏文字色">
+                            <el-form-item :label="t('layouts.Top bar text color')">
                                 <el-color-picker
                                     @change="onCommitState($event, 'headerBarTabColor')"
                                     v-model="configStore.layout.headerBarTabColor"
                                 />
                             </el-form-item>
-                            <el-form-item label="顶栏悬停时背景色">
+                            <el-form-item :label="t('layouts.Background color when hovering over the top bar')">
                                 <el-color-picker
                                     @change="onCommitState($event, 'headerBarHoverBackground')"
                                     v-model="configStore.layout.headerBarHoverBackground"
                                 />
                             </el-form-item>
-                            <el-form-item label="顶栏菜单激活项背景色">
+                            <el-form-item :label="t('layouts.Top bar menu active item background color')">
                                 <el-color-picker
                                     @change="onCommitState($event, 'headerBarTabActiveBackground')"
                                     v-model="configStore.layout.headerBarTabActiveBackground"
                                 />
                             </el-form-item>
-                            <el-form-item label="顶栏菜单激活项文字色">
+                            <el-form-item :label="t('layouts.Top bar menu active item text color')">
                                 <el-color-picker
                                     @change="onCommitState($event, 'headerBarTabActiveColor')"
                                     v-model="configStore.layout.headerBarTabActiveColor"
                                 />
                             </el-form-item>
                         </div>
-                        <el-popconfirm @confirm="restoreDefault" title="确定要恢复全部配置到默认值吗?">
+                        <el-popconfirm
+                            @confirm="restoreDefault"
+                            :title="t('layouts.Are you sure you want to restore all configurations to the default values?')"
+                        >
                             <template #reference>
                                 <div class="ba-center">
-                                    <el-button class="w80" type="info">恢复默认</el-button>
+                                    <el-button class="w80" type="info">{{ t('layouts.Restore default') }}</el-button>
                                 </div>
                             </template>
                         </el-popconfirm>
@@ -176,7 +179,9 @@ import { useRouter } from 'vue-router'
 import selector from '/@/components/icon/selector.vue'
 import { STORE_CONFIG, BEFORE_RESIZE_LAYOUT } from '/@/stores/constant/cacheKey'
 import { Local, Session } from '/@/utils/storage'
+import { useI18n } from 'vue-i18n'
 
+const { t } = useI18n()
 const configStore = useConfig()
 const navTabs = useNavTabs()
 const router = useRouter()
