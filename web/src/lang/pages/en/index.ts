a25f55070b72336e314d52d524fcfc5ab13fd70c
妙码生花 <18523774412@qq.com>
2022-06-23 22:49:12 +0800

    refactor:英文语言包Index改为Home

--- web/src/lang/pages/en/index.ts
+++ web/src/lang/pages/en/index.ts
@@ -2,6 +2,6 @@ export default {
     'Member Center': 'Member Center',
     'Steve Jobs': "A great work of art doesn't have to follow the trend. It can lead the trend itself-- Steve Jobs",
     copyright: 'Copyright',
-    index: 'Index',
+    index: 'Home',
     'language': 'Language',
 }
