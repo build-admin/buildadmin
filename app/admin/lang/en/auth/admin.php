2731686e4fa210ea8c825ef1ceebd7d3d1fae084
gitee-lili <1479968302@qq.com>
2022-07-12 12:22:29 +0800

    完善英文语言包

--- app/admin/lang/en/auth/admin.php
+++ app/admin/lang/en/auth/admin.php
@@ -1,5 +1,5 @@
 <?php
 return [
     'Group Name Arr'                                                           => 'Group',
-    'Please use another administrator account to disable the current account!' => 'Please use another administrator account to disable the current account!',
+    'Please use another administrator account to disable the current account!' => 'It is forbidden to use the current account,please use another administrator account!',
 ];
