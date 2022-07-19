3d209831fbac85eebcefef2ae0155fae641c826e
妙码生花 <18523774412@qq.com>
2022-07-19 11:10:10 +0800

    fix:修复后端默认应用不存在的问题

--- config/app.php
+++ config/app.php
@@ -11,7 +11,7 @@ return [
     // 是否启用路由
     'with_route'       => true,
     // 默认应用
-    'default_app'      => 'index',
+    'default_app'      => 'api',
     // 默认时区
     'default_timezone' => 'Asia/Shanghai',
 
