f506f2873461256f3a0ef641bae3cc18f9c4f5d8
妙码生花 <18523774412@qq.com>
2022-08-12 23:17:47 +0800

    feat:模板市场初始化

---
+++ web/src/views/backend/module/components/header.vue
@@ -0,0 +1,82 @@
+<template>
+    <div>
+        <el-alert class="ba-table-alert" v-if="state.remark" :title="state.remark" type="info" show-icon />
+        <div class="template-header">
+            <div class="table-header-buttons">
+                <el-button :title="$t('refresh')" @click="onRefreshData" v-blur color="#40485b" type="info">
+                    <Icon name="fa fa-refresh" color="#ffffff" size="14" />
+                </el-button>
+                <el-button-group class="ml10">
+                    <el-button title="上传ZIP包安装" v-blur type="primary">
+                        <Icon name="fa fa-upload" color="#ffffff" size="14" />
+                        <span class="table-header-operate-text">上传安装</span>
+                    </el-button>
+                    <el-button title="已上传/安装的模块" v-blur type="primary">
+                        <Icon name="fa fa-desktop" color="#ffffff" size="14" />
+                        <span class="table-header-operate-text">本地模板</span>
+                    </el-button>
+                </el-button-group>
+                <el-button-group class="ml10">
+                    <el-button v-blur type="success">
+                        <Icon name="fa fa-cloud-upload" color="#ffffff" size="14" />
+                        <span class="table-header-operate-text">发布模块</span>
+                    </el-button>
+                    <el-button v-blur type="success">
+                        <Icon name="fa fa-rocket" color="#ffffff" size="14" />
+                        <span class="table-header-operate-text">获得积分</span>
+                    </el-button>
+                </el-button-group>
+
+                <el-button v-blur class="ml10" @click="state.showBaAccount = true" type="success">
+                    <Icon name="fa fa-user-o" color="#ffffff" size="14" />
+                    <span class="table-header-operate-text">会员信息</span>
+                </el-button>
+            </div>
+            <div class="table-search">
+                <el-input v-model="state.params.quickSearch" class="xs-hidden" @input="debounce(onSearchInput, 500)()" placeholder="搜索其实很简单" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { state, loadData } from '../index'
+import { debounce } from '/@/utils/common'
+
+const onRefreshData = () => {
+    for (const key in state.modules) {
+        state.modules[key] = undefined
+    }
+    loadData()
+}
+
+const onSearchInput = () => {
+    state.modules[state.params.activeTab] = undefined
+    loadData()
+}
+</script>
+
+<style scoped lang="scss">
+.ml10 {
+    margin-left: 10px;
+}
+.ba-table-alert {
+    border: none;
+}
+.template-header {
+    display: flex;
+    align-items: center;
+    padding: 10px;
+    margin-bottom: 10px;
+    background-color: var(--color-basic-white);
+    border-radius: var(--el-border-radius-base);
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
+}
+.table-header-operate-text {
+    padding-left: 6px;
+}
+.table-search {
+    margin-left: auto;
+}
+</style>
---
+++ web/src/views/backend/module/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="default-main ba-table-box">
+        <Header />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { state, loadData, loginExpired, showInfo } from './index'
+import Header from './components/header.vue'
+
+</script>
+
+<style scoped lang="scss">
+    
+</style>
