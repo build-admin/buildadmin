c34d8d039debf4ebea01852d20f3df7cfe1f28d1
妙码生花 <18523774412@qq.com>
2022-08-14 13:02:15 +0800

    style:变量已声明未使用时，显示警告而不是错误

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -23,14 +23,14 @@ module.exports = {
         '@typescript-eslint/ban-types': 'off',
         '@typescript-eslint/ban-ts-ignore': 'off',
         '@typescript-eslint/no-unused-vars': [
-            'error',
+            'warn',
             {
                 argsIgnorePattern: '^_',
                 varsIgnorePattern: '^_',
             },
         ],
         'no-unused-vars': [
-            'error',
+            'warn',
             {
                 argsIgnorePattern: '^_',
                 varsIgnorePattern: '^_',
