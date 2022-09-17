f43757a131790e35aa7ae07fb86a48ad5c0e1f6f
妙码生花 <18523774412@qq.com>
2022-09-17 23:31:29 +0800

    refactor:模块状态不为已安装时不定义AppInit事件

--- app/common/service/moduleService.php
+++ app/common/service/moduleService.php
@@ -17,6 +17,9 @@ class moduleService extends Service
     {
         $installed = Server::installedList(root_path() . 'modules' . DIRECTORY_SEPARATOR);
         foreach ($installed as $item) {
+            if ($item['state'] != 1) {
+                continue;
+            }
             $moduleClass = Server::getClass($item['uid']);
             if (class_exists($moduleClass)) {
                 if (method_exists($moduleClass, 'AppInit')) {
