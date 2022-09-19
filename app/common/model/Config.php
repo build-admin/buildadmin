a865241a89b29931527f6ad5c5e989e68f899aa4
妙码生花 <18523774412@qq.com>
2022-09-19 22:05:02 +0800

    feat:增加编程式添加系统配置中的快捷配置入口的方法

--- app/common/model/Config.php
+++ app/common/model/Config.php
@@ -13,33 +13,66 @@ class Config extends Model
      */
     public static function addConfigGroup(string $key, string $value): bool
     {
-        $configGroupRow = adminConfigModel::where('name', 'config_group')->find();
-        foreach ($configGroupRow->value as $item) {
-            if ($item['key'] == $key) {
-                return false;
-            }
-        }
-        $configGroupRow->value = array_merge($configGroupRow->value, [['key' => $key, 'value' => $value]]);
-        $configGroupRow->save();
-        Cache::tag(adminConfigModel::$cacheTag)->clear();
-        return true;
+        return self::addArrayItem('config_group', $key, $value);
     }
 
     /**
      * 删除系统配置分组
      */
-    public static function removeConfigGroup(string $key)
+    public static function removeConfigGroup(string $key): bool
     {
-        $configGroupRow = adminConfigModel::where('name', 'config_group')->find();
-        $configGroup    = $configGroupRow->value;
-        foreach ($configGroup as $iKey => $item) {
+        return self::removeArrayItem('config_group', $key);
+    }
+
+    /**
+     * 添加系统快捷配置入口
+     */
+    public static function addQuickEntrance(string $key, string $value): bool
+    {
+        return self::addArrayItem('config_quick_entrance', $key, $value);
+    }
+
+    /**
+     * 删除系统快捷配置入口
+     */
+    public static function removeQuickEntrance(string $key): bool
+    {
+        return self::removeArrayItem('config_quick_entrance', $key);
+    }
+
+    /**
+     * 为Array类型的配置项添加元素
+     */
+    public static function addArrayItem(string $name, string $key, string $value): bool
+    {
+        $configRow = adminConfigModel::where('name', $name)->find();
+        foreach ($configRow->value as $item) {
             if ($item['key'] == $key) {
-                unset($configGroup[$iKey]);
+                return false;
             }
         }
-        $configGroupRow->value = $configGroup;
-        $configGroupRow->save();
+        $configRow->value = array_merge($configRow->value, [['key' => $key, 'value' => $value]]);
+        $configRow->save();
         Cache::tag(adminConfigModel::$cacheTag)->clear();
+        return true;
+    }
+
+    /**
+     * 删除Array类型配置项的一个元素
+     */
+    public static function removeArrayItem(string $name, string $key): bool
+    {
+        $configRow      = adminConfigModel::where('name', $name)->find();
+        $configRowValue = $configRow->value;
+        foreach ($configRowValue as $iKey => $item) {
+            if ($item['key'] == $key) {
+                unset($configRowValue[$iKey]);
+            }
+        }
+        $configRow->value = $configRowValue;
+        $configRow->save();
+        Cache::tag(adminConfigModel::$cacheTag)->clear();
+        return true;
     }
 
 }
