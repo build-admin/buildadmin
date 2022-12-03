67e084e2e9dafcb1219812554aa0ca9ff9e0efb8
妙码生花 <18523774412@qq.com>
2022-12-04 04:11:32 +0800

    perf:默认关闭监听SQL

--- config/database.php
+++ config/database.php
@@ -53,7 +53,7 @@ return [
             // 是否需要断线重连
             'break_reconnect' => true,
             // 监听SQL
-            'trigger_sql'     => env('app_debug', true),
+            'trigger_sql'     => env('app_debug', false),
             // 开启字段缓存
             'fields_cache'    => false,
         ],
