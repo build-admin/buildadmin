33aa3b79a7edccbc854e7f75bbb45a5d03a67332
妙码生花 <18523774412@qq.com>
2022-12-10 14:02:17 +0800

    fix(CRUD):修复关联表名带下划线生成的代码出错 close #I65DQW

--- app/admin/controller/crud/Crud.php
+++ app/admin/controller/crud/Crud.php
@@ -531,7 +531,7 @@ class Crud extends Backend
                     $this->controllerData['relationVisibleFieldList'][$relationData['relationMethod']] = $relationFields;
                 }
             } elseif ($field['designType'] == 'remoteSelects') {
-                $this->modelData['append'][]  = parse_name($tableName, 0, false);
+                $this->modelData['append'][]  = parse_name($tableName, 1, false);
                 $this->modelData['methods'][] = Helper::assembleStub('mixins/model/getters/remoteSelectLabels', [
                     'field'          => parse_name($tableName, 1),
                     'className'      => str_replace(['.php', '/'], ['', '\\'], '\\' . $field['form']['remote-model']),
