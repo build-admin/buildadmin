b0c83dd6b07258eb41161bc7933f571eb63991ad
妙码生花 <18523774412@qq.com>
2023-01-05 12:08:54 +0800

    perf:先加载语言包再安装路由依赖

--- web/src/main.ts
+++ web/src/main.ts
@@ -14,12 +14,12 @@ import '/@/styles/index.scss'
 
 async function start() {
     const app = createApp(App)
-
-    app.use(router)
     app.use(pinia)
 
     // 全局语言包加载
     const i18n = await loadLang(app)
+
+    app.use(router)
     app.use(ElementPlus, { i18n: i18n.global.t })
 
     // 全局注册
