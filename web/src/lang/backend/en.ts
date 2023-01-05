eb29aa5bc740cce70e75a7e8e8b1fe58a9761c58
妙码生花 <18523774412@qq.com>
2023-01-05 23:49:26 +0800

    perf(lang):终端语言翻译放置到后台公共语言包文件内

--- web/src/lang/backend/en.ts
+++ web/src/lang/backend/en.ts
@@ -1,4 +1,47 @@
 /**
  * backend common language package
  */
-export default {}
+export default {
+    terminal: {
+        Terminal: 'Terminal',
+        'Command run log': 'Command Run Log',
+        'No mission yet': 'There is no task yet',
+        'Test command': 'Test command',
+        'Install dependent packages': 'Install dependent packages',
+        Republish: 'Republish',
+        'Clean up task list': 'Clean up the task list',
+        unknown: 'Unknown',
+        'Waiting for execution': 'Waiting for execution',
+        Connecting: 'Connecting',
+        Executing: 'Executing',
+        'Successful execution': 'Executed successfully',
+        'Execution failed': 'Failed to execute',
+        'Unknown execution result': 'Execution result is unknown',
+        'Are you sure you want to republish?': 'Are you sure to republish?',
+        'Failure to execute this command will block the execution of the queue': 'Failed to execute this command will block queue execution.',
+        'Package manager': 'Package manager',
+        'Please select package manager': 'Please select a package manager',
+        'Switch package manager title':
+            'Readonly WEB terminal, it can more conveniently execute npm install, npm build and other commands after CRUD and other operations. Please select an installed or your favourite NPM package manager below.',
+        'I want to execute the command manually': 'I want to execute the command manually',
+        'Do not refresh the browser': 'Do not refresh your browser.',
+        'Terminal settings': 'Terminal setup',
+        'Install service port': 'Install service port',
+        'The port number to start the installation service (this port needs to be opened for external network access)':
+            'Start the port number of the installation service (External access requires to opened the port for public.)',
+        'Installation service startup command': 'install service start command',
+        'Please execute this command to start the service (add Su under Linux)':
+            'Please execute this command to start the service (add su under Linux)',
+        'Installation service URL': 'Installation service URL',
+        'Please access the site through the installation service URL (except in debug mode)':
+            'Please access the site through the installation service URL (except in debug mode)',
+        'Clean up successful tasks when starting a new task': 'Please clean up successful tasks when starting a new task.',
+        'Back to terminal': 'Back to terminal',
+        or: 'or',
+        'Site domain name': 'Site domain name',
+        'The current terminal is not running under the installation service, and some commands may not be executed':
+            'The current terminal is not running under the installation service, and some commands may not be executed.',
+        'Newly added tasks will never start because they are blocked by failed tasks':
+            'Newly added tasks will never start because they are blocked by failed tasks!(Web terminal)',
+    },
+}
--- web/src/lang/backend/zh-cn.ts
+++ web/src/lang/backend/zh-cn.ts
@@ -2,4 +2,44 @@
  * 后台公共语言包
  * 覆盖风险：请避免使用页面语言包的目录名、文件名作为翻译 key
  */
-export default {}
+export default {
+    terminal: {
+        Terminal: '终端',
+        'Command run log': '命令运行日志',
+        'No mission yet': '还没有任务...',
+        'Test command': '测试命令',
+        'Install dependent packages': '安装依赖包',
+        Republish: '重新发布',
+        'Clean up task list': '清理任务列表',
+        unknown: '未知',
+        'Waiting for execution': '等待执行',
+        Connecting: '连接中...',
+        Executing: '执行中...',
+        'Successful execution': '执行成功',
+        'Execution failed': '执行失败',
+        'Unknown execution result': '执行结果未知',
+        'Are you sure you want to republish?': '确认要重新发布吗？',
+        'Failure to execute this command will block the execution of the queue': '本命令执行失败会阻断队列执行',
+        'Package manager': '包管理器',
+        'Please select package manager': '请选择包管理器',
+        'Switch package manager title':
+            '只读WEB终端，可以在CRUD等操作后方便的执行 npm install、npm build 等命令，请在下方选择一个已安装好或您喜欢的NPM包管理器',
+        'I want to execute the command manually': '我想手动执行命令',
+        'Do not refresh the browser': '请勿刷新浏览器',
+        'Terminal settings': '终端设置',
+        'Install service port': '安装服务端口',
+        'The port number to start the installation service (this port needs to be opened for external network access)':
+            '启动安装服务的端口号(外网访问则需对外开放该端口)',
+        'Installation service startup command': '安装服务启动命令',
+        'Please execute this command to start the service (add Su under Linux)': '请执行本命令以启动服务（Linux下加su）',
+        'Installation service URL': '安装服务URL',
+        'Please access the site through the installation service URL (except in debug mode)': '请通过安装服务Url访问站点（调试模式下例外）',
+        'Clean up successful tasks when starting a new task': '开始新任务时清理已成功任务',
+        'Back to terminal': '回到终端',
+        or: '或',
+        'Site domain name': '站点域名',
+        'The current terminal is not running under the installation service, and some commands may not be executed':
+            '当前终端未运行于安装服务下，部分命令可能无法执行。',
+        'Newly added tasks will never start because they are blocked by failed tasks': '新添加的任务永远不会开始，因为被失败的任务阻塞！（WEB终端）',
+    },
+}
