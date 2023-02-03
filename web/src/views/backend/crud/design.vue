b5abc80a9d1dc0e0f56cd91f6b3fc41300cb319c
Vimin <shusheng732@163.com>
2023-02-03 08:07:39 +0000

    fix(CRUD):修复从历史记录开始时，远程下拉参数无法选择的问题 !85

--- web/src/views/backend/crud/design.vue
+++ web/src/views/backend/crud/design.vue
@@ -665,6 +665,17 @@ const showRemoteSelectPre = (index: number, hideDelField = false) => {
                 state.remoteSelectPre.form.controllerFile = state.fields[index].form['remote-controller'].value
                 state.remoteSelectPre.form.modelFile = state.fields[index].form['remote-model'].value
                 state.remoteSelectPre.form.joinField = state.fields[index].form['relation-fields'].value.split(',')
+                getTableFieldList(state.fields[index].form['remote-table'].value).then((res) => {
+                    const fieldSelect: anyObj = {}
+                    for (const key in res.data.fieldList) {
+                        fieldSelect[key] = (key ? key + ' - ' : '') + res.data.fieldList[key]
+                    }
+                    state.remoteSelectPre.fieldList = fieldSelect
+                })
+                getFileData(state.fields[index].form['remote-table'].value).then((res) => {
+                    state.remoteSelectPre.modelFileList = res.data.modelFileList
+                    state.remoteSelectPre.controllerFileList = res.data.controllerFileList
+                })
             }
         })
         .finally(() => {
