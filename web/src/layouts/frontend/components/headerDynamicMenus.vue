b8b48996d60830fd7cb4c6a3b1cc363794bd5935
妙码生花 <18523774412@qq.com>
2023-05-17 07:45:33 +0800

    style:优化前台动态菜单样式

--- web/src/layouts/frontend/components/headerDynamicMenus.vue
+++ web/src/layouts/frontend/components/headerDynamicMenus.vue
@@ -1,27 +1,25 @@
 <template>
-    <div>
-        <template v-for="(item, idx) in props.menus" :key="idx">
-            <template v-if="!isEmpty(item.children)">
-                <el-sub-menu v-blur :index="`column-${item.id}`">
-                    <template #title>{{ item.title }}</template>
-                    <el-menu-item
-                        v-for="(subItem, subIndex) in item.children"
-                        :key="subIndex"
-                        @click="onClickMenu(subItem)"
-                        v-blur
-                        :index="'column-' + subItem.id"
-                    >
-                        {{ subItem.title }}
-                    </el-menu-item>
-                </el-sub-menu>
-            </template>
-            <template v-else>
-                <el-menu-item @click="onClickMenu(item)" v-blur :index="'column-' + item.id">
-                    {{ item.title }}
+    <template v-for="(item, idx) in props.menus" :key="idx">
+        <template v-if="!isEmpty(item.children)">
+            <el-sub-menu v-blur :index="`column-${item.id}`">
+                <template #title>{{ item.title }}</template>
+                <el-menu-item
+                    v-for="(subItem, subIndex) in item.children"
+                    :key="subIndex"
+                    @click="onClickMenu(subItem)"
+                    v-blur
+                    :index="'column-' + subItem.id"
+                >
+                    {{ subItem.title }}
                 </el-menu-item>
-            </template>
+            </el-sub-menu>
         </template>
-    </div>
+        <template v-else>
+            <el-menu-item @click="onClickMenu(item)" v-blur :index="'column-' + item.id">
+                {{ item.title }}
+            </el-menu-item>
+        </template>
+    </template>
 </template>
 
 <script setup lang="ts">
