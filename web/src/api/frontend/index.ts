5cf0fc953093b76af188b254ff8d80473d00cc7d
妙码生花 <18523774412@qq.com>
2023-05-17 15:21:32 +0800

    perf:前台初始化请求防抖

--- web/src/api/frontend/index.ts
+++ web/src/api/frontend/index.ts
@@ -1,32 +1,34 @@
 import createAxios from '/@/utils/axios'
 import { useSiteConfig } from '/@/stores/siteConfig'
 import { useMemberCenter } from '/@/stores/memberCenter'
-import { setTitle } from '/@/utils/common'
+import { setTitle, debounce } from '/@/utils/common'
 import { handleFrontendRoute } from '/@/utils/router'
 
 const controllerUrl = '/api/index/'
 
 /**
- * 前端初始化请求，获取站点配置信息，动态路由信息等（非会员中心初始化请求，它们是分开的）
+ * 前台初始化请求，获取站点配置信息，动态路由信息等（非会员中心初始化请求，它们是分开的）
  */
 export function index() {
-    const siteConfig = useSiteConfig()
-    const memberCenter = useMemberCenter()
+    debounce(() => {
+        const siteConfig = useSiteConfig()
+        const memberCenter = useMemberCenter()
 
-    if (siteConfig.siteName) {
-        return
-    }
-
-    return createAxios({
-        url: controllerUrl + 'index',
-        method: 'get',
-    }).then((res) => {
-        setTitle(res.data.site.siteName)
-        if (res.data.rules) {
-            handleFrontendRoute(res.data.rules)
+        if (siteConfig.siteName) {
+            return
         }
-        siteConfig.dataFill(res.data.site)
-        memberCenter.setStatus(res.data.openMemberCenter)
-        if (!res.data.openMemberCenter) memberCenter.setLayoutMode('Disable')
-    })
+
+        return createAxios({
+            url: controllerUrl + 'index',
+            method: 'get',
+        }).then((res) => {
+            setTitle(res.data.site.siteName)
+            if (res.data.rules) {
+                handleFrontendRoute(res.data.rules)
+            }
+            siteConfig.dataFill(res.data.site)
+            memberCenter.setStatus(res.data.openMemberCenter)
+            if (!res.data.openMemberCenter) memberCenter.setLayoutMode('Disable')
+        })
+    }, 100)()
 }
