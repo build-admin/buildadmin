d887fe8e70ea205a56daddc9be89a0c1e3065707
妙码生花 <18523774412@qq.com>
2023-05-24 01:51:03 +0800

    perf:因 web-nuxt 提高 API 节流阈值

--- config/throttle.php
+++ config/throttle.php
@@ -15,7 +15,7 @@ return [
     // 要被限制的请求类型, eg: GET POST PUT DELETE HEAD 等
     'visit_method'                 => ['GET', 'HEAD'],
     // 设置访问频率，例如 '10/m' 指的是允许每分钟请求10次;'10/60'指允许每60秒请求10次。值 null 表示不限制， eg: null 10/m  20/h  300/d 200/300
-    'visit_rate'                   => '70/m',
+    'visit_rate'                   => '120/m',
     /*
      * 设置节流算法，组件提供了四种算法：
      *  - CounterFixed ：计数固定窗口
