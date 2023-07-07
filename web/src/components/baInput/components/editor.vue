a71cc2ce08ed2838e5738474e7af6cb1e6288470
妙码生花 <18523774412@qq.com>
2023-07-07 20:21:32 +0800

    feat(baInput):多富文本编辑器共存支持

--- web/src/components/baInput/components/editor.vue
+++ web/src/components/baInput/components/editor.vue
@@ -1,11 +1,39 @@
+<!-- 多编辑器共存支持 -->
+<!-- 所有编辑器的代码位于 /@/components/mixins/editor 文件夹，一个文件为一种编辑器，文件名则为编辑器名称 -->
+<!-- 向本组件传递 editorType（文件名/编辑器名称）自动加载对应的编辑器进行渲染 -->
 <template>
-    <div class="tips">{{ $t('utils.Please install editor') }}</div>
+    <div>
+        <component v-bind="$attrs" :is="mixins[state.editorType]" />
+    </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive } from 'vue'
+import type { Component } from 'vue'
 
-<style scoped lang="scss">
-.tips {
-    color: var(--el-text-color-placeholder);
+interface Props {
+    editorType?: string
 }
-</style>
+
+const props = withDefaults(defineProps<Props>(), {
+    editorType: 'default',
+})
+
+const state = reactive({
+    editorType: props.editorType,
+})
+
+const mixins: Record<string, Component> = {}
+const mixinComponents: Record<string, any> = import.meta.glob('../../mixins/editor/**.vue', { eager: true })
+for (const key in mixinComponents) {
+    const fileName = key.replace('../../mixins/editor/', '').replace('.vue', '')
+    mixins[fileName] = mixinComponents[key].default
+
+    // 未安装富文本编辑器时，值为 default，安装之后，则值为最后一个编辑器的名称
+    if (props.editorType == 'default' && fileName != 'default') {
+        state.editorType = fileName
+    }
+}
+</script>
+
+<style scoped lang="scss"></style>
---
+++ web/src/components/mixins/editor/default.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="tips">{{ $t('utils.Please install editor') }}</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.tips {
+    color: var(--el-text-color-placeholder);
+}
+</style>
