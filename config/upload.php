e95d03fa0b3ae4b4d9edac2673663b3cbb670071
妙码生花 <18523774412@qq.com>
2023-07-12 20:36:51 +0800

    perf:上传文件使用部分文件名作为前缀以便识别

--- config/upload.php
+++ config/upload.php
@@ -7,7 +7,7 @@ return [
     // 最大上传
     'maxsize'  => '10mb',
     // 文件保存格式化方法
-    'savename' => '/storage/{topic}/{year}{mon}{day}/{filesha1}{.suffix}',
+    'savename' => '/storage/{topic}/{year}{mon}{day}/{filename}{filesha1}{.suffix}',
     // 文件格式限制
     'mimetype' => 'jpg,png,bmp,jpeg,gif,webp,zip,rar,xls,xlsx,doc,docx,wav,mp4,mp3,txt',
 ];
