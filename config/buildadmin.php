ccefefc638e46222fb2a0e9fb5fa7fc17ba8b0a4
妙码生花 <18523774412@qq.com>
2023-07-31 13:58:59 +0800

    feat:发行为 v2.0.1

--- config/buildadmin.php
+++ config/buildadmin.php
@@ -68,7 +68,7 @@ return [
     // 内容分发网络URL，末尾不带`/`
     'cdn_url'              => '',
     // 版本号
-    'version'              => 'v2.0.0',
+    'version'              => 'v2.0.1',
     // 接口地址
     'api_url'              => 'https://buildadmin.com',
 ];
