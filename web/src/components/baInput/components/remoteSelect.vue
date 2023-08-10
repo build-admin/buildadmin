3e647fe36b9890c87105d6e3d35001708c3e8bc0
妙码生花 <18523774412@qq.com>
2023-08-10 18:17:43 +0800

    fix(baInput):修复远程下拉脱焦后会有个多余的请求的问题

--- web/src/components/baInput/components/remoteSelect.vue
+++ web/src/components/baInput/components/remoteSelect.vue
@@ -183,8 +183,10 @@ const onClear = () => {
 }
 
 const onLogKeyword = (q: string) => {
-    state.keyword = q
-    getData()
+    if (state.keyword != q) {
+        state.keyword = q
+        getData()
+    }
 }
 
 const getData = (initValue: valType = '') => {
