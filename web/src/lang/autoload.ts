5c1cbec56f39fafca72a31bc2b595f5729ad128f
妙码生花 <18523774412@qq.com>
2023-11-12 19:47:25 +0800

    refactor:语言包按需加载映射表中的后台入口路径由字面量改为变量

--- web/src/lang/autoload.ts
+++ web/src/lang/autoload.ts
@@ -1,12 +1,14 @@
+import { adminBaseRoutePath } from '/@/router/static/adminBase'
+
 /*
  * 语言包按需加载映射表
  * 使用固定字符串 ${lang} 指代当前语言
- * key为页面path，value为语言包文件相对路径，访问时，按需自动加载映射表的语言包，同时加载path对应的语言包（若存在）
+ * key 为页面 path，value 为语言包文件相对路径，访问时，按需自动加载映射表的语言包，同时加载 path 对应的语言包（若存在）
  */
 export default {
     '/': ['./frontend/${lang}/index.ts'],
-    '/admin/moduleStore': ['./backend/${lang}/module.ts'],
-    '/admin/user/rule': ['./backend/${lang}/auth/rule.ts'],
-    '/admin/user/scoreLog': ['./backend/${lang}/user/moneyLog.ts'],
-    '/admin/crud/crud': ['./backend/${lang}/crud/log.ts', './backend/${lang}/crud/state.ts'],
+    [adminBaseRoutePath + '/moduleStore']: ['./backend/${lang}/module.ts'],
+    [adminBaseRoutePath + '/user/rule']: ['./backend/${lang}/auth/rule.ts'],
+    [adminBaseRoutePath + '/user/scoreLog']: ['./backend/${lang}/user/moneyLog.ts'],
+    [adminBaseRoutePath + '/crud/crud']: ['./backend/${lang}/crud/log.ts', './backend/${lang}/crud/state.ts'],
 }
