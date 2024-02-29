c95fc126d07a4f013256b0c98f3a4ffd360c9ebc
2368302435@qq.com <2368302435@qq.com>
2024-02-29 16:01:50 +0800

    feat:增加后台公共的获取数据表列表接口备用

--- app/admin/controller/Ajax.php
+++ app/admin/controller/Ajax.php
@@ -66,7 +66,7 @@ class Ajax extends Backend
      * 获取已脱敏的数据库连接配置列表
      * @throws Throwable
      */
-    public function getDatabaseConnections(): void
+    public function getDatabaseConnectionList(): void
     {
         $connections     = config('database.connections');
         $desensitization = [];
@@ -101,6 +101,39 @@ class Ajax extends Backend
 
         $tablePk = Db::table($table)->getPk();
         $this->success('', ['pk' => $tablePk]);
+    }
+
+    /**
+     * 获取数据表列表
+     * @throws Throwable
+     */
+    public function getTableList(): void
+    {
+        $connection   = $this->request->request('connection');// 数据库连接配置标识
+        $samePrefix   = $this->request->request('samePrefix/b', true);// 是否仅返回项目数据表（前缀同项目一致的）
+        $excludeTable = $this->request->request('excludeTable/a', []);// 要排除的数据表数组（表名无需带前缀）
+
+        $outTables = [];
+        $dbConfig  = TableManager::getConnectionConfig($connection);
+        $tables    = TableManager::getTableList($connection);
+        $pattern   = '/^' . $dbConfig['prefix'] . '/i';
+        foreach ($tables as $table => $comment) {
+            if ($samePrefix && !preg_match($pattern, $table)) continue;
+
+            $table = preg_replace($pattern, '', $table);
+            if (!in_array($table, $excludeTable)) {
+                $outTables[] = [
+                    'table'      => $table,
+                    'comment'    => $comment,
+                    'connection' => $connection,
+                    'prefix'     => $dbConfig['prefix'],
+                ];
+            }
+        }
+
+        $this->success('', [
+            'list' => $outTables,
+        ]);
     }
 
     /**
