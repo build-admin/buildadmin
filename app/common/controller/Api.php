74d4a66b62beb2e10b54b36e497cf6ef1b532db5
妙码生花 <18523774412@qq.com>
2024-03-12 00:43:40 +0800

    refactor:优化 API 控制器基类的类型定义

--- app/common/controller/Api.php
+++ app/common/controller/Api.php
@@ -69,7 +69,7 @@ class Api extends BaseController
      * @param array       $header  发送的 header 信息
      * @param array       $options Response 输出参数
      */
-    protected function success(string $msg = '', mixed $data = null, int $code = 1, string $type = null, array $header = [], array $options = [])
+    protected function success(string $msg = '', mixed $data = null, int $code = 1, string $type = null, array $header = [], array $options = []): void
     {
         $this->result($msg, $data, $code, $type, $header, $options);
     }
@@ -83,7 +83,7 @@ class Api extends BaseController
      * @param array       $header  发送的 header 信息
      * @param array       $options Response 输出参数
      */
-    protected function error(string $msg = '', mixed $data = null, int $code = 0, string $type = null, array $header = [], array $options = [])
+    protected function error(string $msg = '', mixed $data = null, int $code = 0, string $type = null, array $header = [], array $options = []): void
     {
         $this->result($msg, $data, $code, $type, $header, $options);
     }
