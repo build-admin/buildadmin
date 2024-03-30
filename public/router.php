a7940c97c999c847084d71c97a981bfe68ab2cda
妙码生花 <18523774412@qq.com>
2024-03-30 23:50:33 +0800

    fix:内置开发服务总是禁用输出压缩

--- public/router.php
+++ public/router.php
@@ -10,6 +10,8 @@
 // +----------------------------------------------------------------------
 // $Id$
 
+@ini_set('zlib.output_compression', 'Off');
+
 if (is_file($_SERVER["DOCUMENT_ROOT"] . $_SERVER["SCRIPT_NAME"])) {
     return false;
 } else {
