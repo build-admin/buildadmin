27ecbffdfe2cf3792e198fe394ade748c95f0056
妙码生花 <18523774412@qq.com>
2024-06-25 17:03:05 +0800

    refactor(baInput):优化 FormItem 组件的类型定义

--- web/src/components/formItem/index.ts
+++ web/src/components/formItem/index.ts
@@ -1,17 +1,9 @@
 import type { CSSProperties } from 'vue'
-import type { FormItemRule } from 'element-plus'
+import type { FormItemProps } from 'element-plus'
 
-export interface FormItemAttr {
+export interface FormItemAttr extends Partial<FormItemProps> {
     id?: string
     class?: string
-    prop?: string | string[]
-    labelWidth?: string | number
-    required?: boolean
-    rules?: FormItemRule | FormItemRule[]
-    error?: string
-    showMessage?: boolean
-    inlineMessage?: boolean
-    size?: 'large' | 'default' | 'small'
     style?: CSSProperties
     blockHelp?: string
 }
