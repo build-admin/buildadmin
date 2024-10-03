43e42254e14ea0a047d650a45e110d9cf3bf3ae7
妙码生花 <18523774412@qq.com>
2024-10-03 09:01:50 +0800

    refactor(BaTable):优化类型定义

--- web/types/table.d.ts
+++ web/types/table.d.ts
@@ -1,17 +1,15 @@
-import type { TagProps, ButtonType, FormInstance, ButtonProps, TableColumnCtx, ColProps } from 'element-plus'
-import { Component, ComponentPublicInstance } from 'vue'
+import type { ButtonProps, ButtonType, ColProps, FormInstance, PopconfirmProps, TableColumnCtx, TagProps } from 'element-plus'
+import type { Mutable } from 'element-plus/es/utils'
+import type { Component, ComponentPublicInstance } from 'vue'
 import Table from '/@/components/table/index.vue'
-import type { PopconfirmProps } from 'element-plus'
-import { Mutable } from 'element-plus/es/utils'
 
 declare global {
-    /* baTable */
     interface BaTable {
-        // 表格 ref，通常在 页面onMounted 时赋值
+        // 表格 ref，通常在 页面 onMounted 时赋值，可选的
         ref?: typeof Table
         // 表格对应数据表的主键字段
         pk?: string
-        // 表格数据，通过getIndex加载
+        // 表格数据，通过 getIndex 加载
         data?: TableRow[]
         // 路由 remark，后台菜单规则备注信息
         remark?: string | null
@@ -23,7 +21,7 @@ declare global {
         column: TableColumn[]
         // 数据总量
         total?: number
-        // 字段搜索,快速搜索,分页等数据
+        // 字段搜索，快速搜索，分页等数据
         filter?: {
             page?: number
             limit?: number
@@ -34,19 +32,19 @@ declare global {
         }
         // 默认排序字段和排序方式
         defaultOrder?: { prop: string; order: string }
-        // 拖动排序限位字段:例如拖动行pid=1,那么拖动目的行pid也需要为1
+        // 拖动排序限位字段，例如拖动行 pid=1，那么拖动目的行 pid 也需要为 1
         dragSortLimitField?: string
-        // 接受url的query参数并自动触发通用搜索
+        // 接受 url 的 query 参数并自动触发通用搜索
         acceptQuery?: boolean
         // 显示公共搜索
         showComSearch?: boolean
-        // 不需要'双击编辑'的字段，type=selection的列为 undefined
+        // 不需要 '双击编辑' 的字段，type=selection 的列为 undefined
         dblClickNotEditColumn?: (string | undefined)[]
         // 是否展开所有子项，树状表格专用属性
         expandAll?: boolean
         // 当前表格所在页面的路由 path
         routePath?: string
-        // 表格扩展数据，随意定义，以便一些自定义数据可以随baTable实例传递
+        // 表格扩展数据，随意定义，以便一些自定义数据可以随 baTable 实例传递
         extend?: anyObj
     }
 
@@ -55,32 +53,35 @@ declare global {
             renderValue: any
             renderRow: TableRow
             renderField: TableColumn
+            renderColumn: TableColumnCtx<TableRow>
+            renderIndex: number
         }
     }
 
-    /* baTableForm */
     interface BaTableForm {
-        // 表单ref，实例化表格时通常无需传递
+        // 表单 ref，实例化表格时通常无需传递
         ref?: FormInstance | undefined
-        // 表单项label的宽度
+        // 表单项 label 的宽度
         labelWidth?: number
         // 当前操作:add=添加,edit=编辑
         operate?: string
-        // 被操作数据ID,支持批量编辑:add=[0],edit=[1,2,n]
+        // 被操作数据ID，支持批量编辑:add=[0],edit=[1,2,n]
         operateIds?: string[]
         // 表单数据，内含用户输入
         items?: anyObj
         // 提交按钮状态
         submitLoading?: boolean
-        // 默认表单数据(添加时)
+        // 默认表单数据（添加时）
         defaultItems?: anyObj
         // 表单加载状态
         loading?: boolean
-        // 表单扩展数据，随意定义，以便一些自定义数据可以随baTable实例传递
+        // 表单扩展数据，随意定义，以便一些自定义数据可以随 baTable 实例传递
         extend?: anyObj
     }
 
-    /* BaTable前置处理函数(前置埋点) */
+    /**
+     * BaTable 前置处理函数（前置埋点）
+     */
     interface BaTableBefore {
         // 获取表格数据前
         getIndex?: () => boolean | void
@@ -103,7 +104,9 @@ declare global {
         [key: string]: Function | undefined
     }
 
-    /* BaTable后置处理函数(后置埋点) */
+    /**
+     * BaTable 后置处理函数（后置埋点）
+     */
     interface BaTableAfter {
         // 表格数据请求后
         getIndex?: ({ res }: { res: ApiResponse }) => void
@@ -129,45 +132,41 @@ declare global {
         fieldData: Map<string, any>
     }
 
-    /* 表格列 */
+    /**
+     * 表格列
+     */
     interface TableColumn extends Partial<TableColumnCtx<TableRow>> {
         // 是否显示
         show?: boolean
-        // 是否在下拉菜单的复选框显示 默认为true显示
-        enableColumnDisplayControl?: boolean
-        // 渲染为 tableRenderer 之一
+        // 渲染为 \components\table\fieldRender\ 中的组件（单元格渲染器）之一，填写组件名即可
         render?: tableRenderer
+        // 自定义插槽渲染（render: 'slot'）时，slot 的名称
+        slotName?: string
+        // 自定义组件/函数渲染（render: 'customRender'）时，要渲染的组件或已注册组件名称的字符串
+        customRender?: string | Component
+        // 渲染为 tag 时 el-tag 组件的 effect
+        effect?: TagProps['effect']
+        // 渲染为 tag 时 el-tag 组件的 size
+        size?: TagProps['size']
+        // 单元格渲染器需要的自定义数据，比如（render: 'tag'）时，可以指定不同值时的 tag 的 type 属性 { open: 'success', close: 'info' }
+        custom?: any
+        // 渲染为链接时，链接的打开方式
+        target?: aTarget
+        // 渲染为 datetime 时的格式化方式，字母可以自由组合:y=年,m=月,d=日,h=时,M=分,s=秒，默认：yyyy-mm-dd hh:MM:ss
+        timeFormat?: string
+        // 默认值（单元格值为 undefined 或 null 时取默认值，仅使用了 render 时有效）
+        default?: any
+        // 值替换数据，如 { open: '开', close: '已关闭' }
+        replaceValue?: any
         // 操作按钮组
         buttons?: OptButton[]
-        // 渲染为Tag时:el-tag 组件的主题
-        effect?: TagProps['effect']
-        // 渲染为Tag时:el-tag 组件的size
-        size?: TagProps['size']
-        // 自定义数据:比如渲染为Tag时,可以指定不同值时的Tag的Type属性 { open: 'success', close: 'info' }
-        custom?: any
-        // 谨慎使用：自定义渲染模板，方法可返回html内容，请确保返回内容是xss安全的
-        customTemplate?: (row: TableRow, field: TableColumn, value: any, column: TableColumnCtx<TableRow>, index: number) => string
-        // 自定义组件/函数渲染
-        customRender?: string | Component
-        // 自定义渲染为 slot 时，slot的名称
-        slotName?: string
-        // 渲染为链接时,链接的打开方式
-        target?: aTarget
-        // 渲染为:url 时的点击事件
-        click?: (row: TableRow, field: TableColumn, value: any, column: TableColumnCtx<TableRow>, index: number) => any
-        // 渲染为 datetime 时的格式化方式,字母可以自由组合:y=年,m=月,d=日,h=时,M=分,s=秒，默认：yyyy-mm-dd hh:MM:ss
-        timeFormat?: string
-        // 默认值
-        default?: any
-        // 值替换数据,如{open: '开'}
-        replaceValue?: any
-        // 使用了 render 属性时,渲染前对字段值的预处理方法，请返回新值
-        renderFormatter?: (row: TableRow, field: TableColumn, value: any, column: TableColumnCtx<TableRow>, index: number) => any
-        // 通用搜索操作符,默认值为=,false禁用此字段通用搜索,支持的操作符见下定义
+        // 是否允许动态控制字段是否显示，默认为 true
+        enableColumnDisplayControl?: boolean
+        // 通用搜索操作符，默认值为=，值为 false 禁用此字段通用搜索，支持的操作符见下类型定义
         operator?: boolean | OperatorStr
-        // 通用搜索框的placeholder
+        // 通用搜索框的 placeholder
         operatorPlaceholder?: string
-        // 公共搜索渲染方式:上方的 render=tag|switch 时公共搜索也会渲染为下拉，数字会渲染为范围筛选，时间渲染为时间选择器等
+        // 公共搜索渲染方式，render=tag|switch 时公共搜索也会渲染为下拉，数字会渲染为范围筛选，时间渲染为时间选择器等
         comSearchRender?: 'remoteSelect' | 'select' | 'date' | 'customRender' | 'slot'
         // 公共搜索自定义组件/函数渲染
         comSearchCustomRender?: string | Component
@@ -185,16 +184,23 @@ declare global {
             multiple?: boolean
             remoteUrl: string
         }
+        // 谨慎使用：自定义渲染模板，方法可返回 html 内容，请确保返回内容是 xss 安全的
+        customTemplate?: (row: TableRow, field: TableColumn, value: any, column: TableColumnCtx<TableRow>, index: number) => string
         // 单元格渲染组件的 key，此 key 值改变时单元格将自动重新渲染，默认将根据列配置等属性自动生成
         getRenderKey?: (row: TableRow, field: TableColumn, column: TableColumnCtx<TableRow>, index: number) => string
+        // 使用了 render 属性时，渲染前对字段值的预处理方法（即将废弃，请使用兼容 el-table 的 formatter 函数代替）
+        renderFormatter?: (row: TableRow, field: TableColumn, value: any, column: TableColumnCtx<TableRow>, index: number) => any
+        // 渲染为 url 时的点击事件（即将废弃，请使用 el-table 的 @cell-click 或单元格自定义渲染代替）
+        click?: (row: TableRow, field: TableColumn, value: any, column: TableColumnCtx<TableRow>, index: number) => any
     }
 
-    /* 表格右侧操作按钮 */
+    /**
+     * 表格右侧操作按钮
+     */
     interface OptButton {
         // 渲染方式:tipButton=带tip的按钮,confirmButton=带确认框的按钮,moveButton=移动按钮,basicButton=普通按钮
         render: 'tipButton' | 'confirmButton' | 'moveButton' | 'basicButton'
         name: string
-        // 自动使用 t() 包裹，请使用语言翻译key，比如 admin.infoBtn
         title?: string
         text?: string
         class?: string
@@ -204,23 +210,29 @@ declare global {
         disabledTip?: boolean
         // 自定义点击事件
         click?: (row: TableRow, field: TableColumn) => void
-        // 按钮是否显示，请返回布尔值
+        // 按钮是否显示，请返回布尔值，比如：display: auth('add')
         display?: (row: TableRow, field: TableColumn) => boolean
         // 按钮是否禁用，请返回布尔值
         disabled?: (row: TableRow, field: TableColumn) => boolean
-        // 自定义el-button属性
+        // 自定义其他 el-button 的属性
         attr?: Partial<Mutable<ButtonProps>>
     }
 
-    /* 表格行 */
+    /**
+     * 表格行
+     */
     interface TableRow extends anyObj {
         children?: TableRow[]
     }
 
-    /* 表头支持的按钮 */
+    /**
+     * 表头支持的按钮
+     */
     type HeaderOptButton = 'refresh' | 'add' | 'edit' | 'delete' | 'unfold' | 'comSearch' | 'quickSearch' | 'columnDisplay'
 
-    /* 通用搜索操作符支持的值 */
+    /**
+     * 通用搜索操作符支持的值
+     */
     type OperatorStr =
         | 'eq' // 等于，默认值
         | 'ne' // 不等于
@@ -232,7 +244,7 @@ declare global {
         | 'NOT LIKE'
         | 'IN'
         | 'NOT IN'
-        | 'RANGE' // 范围，将生成两个输入框 以输入最小值和最大值
+        | 'RANGE' // 范围，将生成两个输入框，可以输入最小值和最大值
         | 'NOT RANGE'
         | 'NULL' // 是否为NULL，将生成单个复选框
         | 'NOT NULL'
@@ -245,10 +257,14 @@ declare global {
         | '<'
         | '<='
 
-    /* 链接打开方式 */
+    /**
+     * 链接打开方式
+     */
     type aTarget = '_blank' | '_self'
 
-    /* 通用搜索事件返回的Data */
+    /**
+     * 通用搜索事件返回的 Data
+     */
     interface comSearchData {
         field: string
         val: string | string[] | number | number[]
